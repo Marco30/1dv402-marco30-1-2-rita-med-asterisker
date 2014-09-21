4ebf4a09e8578f2678d109086c42e1f8b4790970
marco30 <mv222fp@student.lnu.se>
2014-09-21 20:37:28 +0200

    'lab1.2'

--- 1DV402.S1.2.L01A/1DV402.S1.2.L01A/Program.cs
+++ 1DV402.S1.2.L01A/1DV402.S1.2.L01A/Program.cs
@@ -12,9 +12,21 @@ namespace _1DV402.S1._2.L01A
         {
 
             string indata;
-
             Console.WriteLine("hej");
             Console.WriteLine("vad gör du");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(" "); 
+            Console.Write("* "); 
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write(" "); 
+            Console.Write("* "); 
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" ");
+            Console.Write("* ");
+            Console.WriteLine();
 
             indata = Console.ReadLine();
         }
