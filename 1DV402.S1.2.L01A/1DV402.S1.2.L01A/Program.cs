23c150b10da77412192cbde3baeb21903f6d0cd0
marco30 <mv222fp@student.lnu.se>
2014-09-21 19:43:15 +0200

    'test'

---
+++ 1DV402.S1.2.L01A/1DV402.S1.2.L01A/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S1._2.L01A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string indata;
+
+            Console.WriteLine("hej");
+            Console.WriteLine("vad gör du");
+
+            indata = Console.ReadLine();
+        }
+    }
+}
